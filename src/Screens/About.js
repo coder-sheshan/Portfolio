e294af82a1cdff407b8cee6cdc91ff33e1cd997a
sheshanraj-habits <132676263+sheshanraj-habits@users.noreply.github.com>
2023-09-26 16:07:33 +0530

    "React Video Player Added"

--- src/Screens/About.js
+++ src/Screens/About.js
@@ -1,49 +1,48 @@
-import React from 'react';
-import Coding from '../Assets/coding-min.jpg'
+import React from "react";
+import CodingVideo from "../Assets/codingvid.mp4";
+import ReactPlayer from "react-player";
 
 const About = () => {
-    return (
-        <div>
-            <section id='about'>
-                <h1 class="text-indigo-500 font-semibold sm:text-3xl text-2xl align-middle justify-center flex">About Me</h1>
-                <div
-                    class="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8"
-                >
-                    <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-                        <div
-                            class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full"
-                        >
-                            <img
-                                alt="Party"
-                                // src="https://images.unsplash.com/photo-1527529482837-4698179dc6ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                                src={Coding}     
-                                class="absolute inset-0 h-full w-full object-cover"
-                            />
-                        </div>
+  return (
+    <div>
+      <section id="about">
+        <h1 class="text-indigo-500 font-semibold sm:text-3xl text-2xl align-middle justify-center flex">
+          About Me
+        </h1>
+        <div class="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
+          <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+            <div class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+              <ReactPlayer
+                playing={true}
+                loop={true}
+                controls={true}
+                url={CodingVideo}
+              />
+            </div>
 
-                        <div class="lg:py-20">
-                            <h2 class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-xl font-bold text-transparent sm:text-3xl">Full Stack Develper</h2>
+            <div class="lg:py-20">
+              <h2 class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-xl font-bold text-transparent sm:text-3xl">
+                Full Stack Develper
+              </h2>
 
-                            <p class="mt-4 text-white text-justify">
-                                I'm Sheshan Raj V and I am currently majoring in the field of Computer Science. I love the field of computer science as it
-                                involves bit of every other field. I love learning new technologies. I am a budding MERN full-stack developer. I have now
-                                completed the Full-Stack course. I have done many projects which are listed below, which gave me
-                                professional experience in the field of computer science. I am also familiar with the range of Python.I worked on a (Habits
-                                Financial App) mobile app project in React Native as well as a shopping cart E-Commerce project in Next.js.
-                            </p>
-
-                            {/* <a
-                                href="#"
-                                class="mt-8 inline-block rounded bg-purple-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-purple-800 focus:outline-none focus:ring focus:ring-yellow-400"
-                            >
-                                Get Started Today
-                            </a> */}
-                        </div>
-                    </div>
-                </div>
-            </section>
+              <p class="mt-4 text-white text-justify">
+                I'm Sheshan Raj V and I am currently majoring in the field of
+                Computer Science. I love the field of computer science as it
+                involves bit of every other field. I love learning new
+                technologies. I am a budding MERN full-stack developer. I have
+                now completed the Full-Stack course. I have done many projects
+                which are listed below, which gave me professional experience in
+                the field of computer science. I am also familiar with the range
+                of Python.I worked on a (Habits Financial App) mobile app
+                project in React Native as well as a shopping cart E-Commerce
+                project in Next.js.
+              </p>
+            </div>
+          </div>
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/Screens/Projects.js
+++ src/Screens/Projects.js
@@ -1,224 +1,175 @@
-import React from 'react';
-import Project_Img1 from '../Assets/Project1.png';
-import Project_IMg2 from '../Assets/projects2.png';
-import project_Todo from '../Assets/Todo.png';
-import LogIn from '../Assets/login.png';
-import Habits from '../Assets/habits.png'
-import WorkInProgress from '../Assets/WIP.jpg';
-import Netflix from '../Assets/netflix.png';
-import Kavis from '../Assets/kavis.png';
-import eCommerce from '../Assets/ecommerce.png';
+import React from "react";
+import Project_Img1 from "../Assets/Project1.png";
+import Project_IMg2 from "../Assets/projects2.png";
+import project_Todo from "../Assets/Todo.png";
+import LogIn from "../Assets/login.png";
+import Habits from "../Assets/habits.png";
+import WorkInProgress from "../Assets/WIP.jpg";
+import Netflix from "../Assets/netflix.png";
+import Kavis from "../Assets/kavis.png";
+import eCommerce from "../Assets/ecommerce.png";
 
 const Projects = () => {
-    return (
-        <div>
-            <section id='projects' class="bg-gray-900 text-white">
-                <div
-                    class="mx-auto  px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8"
-                >
-                    <div class="mx-auto max-w-lg text-center">
-                        <h2 class="text-2xl text-indigo-500 font-semibold sm:text-3xl">My Projects</h2>
-                    </div>
+  return (
+    <div>
+      <section id="projects" class="bg-gray-900 text-white">
+        <div class="mx-auto  px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
+          <div class="mx-auto max-w-lg text-center">
+            <h2 class="text-2xl text-indigo-500 font-semibold sm:text-3xl">
+              My Projects
+            </h2>
+          </div>
 
-                    <div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-                        <a href="https://sheshan-raj-cb9cf3.netlify.app/" target='blank' rel="noreferrer" class="block">
-                            <img
-                                alt="Signage"
-                                src={Project_Img1}
-                                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                            />
+          <div class="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+            <a
+              href="https://sheshan-raj-cb9cf3.netlify.app/"
+              target="blank"
+              rel="noreferrer"
+              class="block"
+            >
+              <img
+                alt="Signage"
+                src={Project_Img1}
+                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
+              />
 
-                            <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                                <strong class="font-medium">Bootstrap</strong>
+              <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
+                <strong class="font-medium">Bootstrap</strong>
 
-                                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
 
-                                <p class="mt-0.5 opacity-50 sm:mt-0">Website / Bootstrap</p>
-                            </div>
-                        </a>
+                <p class="mt-0.5 opacity-50 sm:mt-0">Website / Bootstrap</p>
+              </div>
+            </a>
 
-                        <a href="https://portfolio-nu-one-34.vercel.app/" target='blank' rel="noreferrer" class="block">
-                            <img
-                                alt="Signage"
-                                src={Project_IMg2}
-                                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                            />
+            <a
+              href="https://portfolio-nu-one-34.vercel.app/"
+              target="blank"
+              rel="noreferrer"
+              class="block"
+            >
+              <img
+                alt="Signage"
+                src={Project_IMg2}
+                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
+              />
 
-                            <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                                <strong class="font-medium">Tailwind Css</strong>
+              <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
+                <strong class="font-medium">Tailwind Css</strong>
 
-                                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
 
-                                <p class="mt-0.5 opacity-50 sm:mt-0">Portfolio / Email.js</p>
-                            </div>
-                        </a>
+                <p class="mt-0.5 opacity-50 sm:mt-0">Portfolio / Email.js</p>
+              </div>
+            </a>
+            {/* phrase=====2 */}
+            <a
+              href="https://mern-jwt-token.vercel.app/"
+              target="blank"
+              rel="noreferrer"
+              class="block"
+            >
+              <img
+                alt="Signage"
+                src={LogIn}
+                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
+              />
 
-                        {/* <a href="https://sheshan-raj-1d655d.netlify.app/" target='blank' rel="noreferrer" class="block">
-                            <img
-                                alt="Signage"
-                                src={project_Todo}
-                                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                            />
+              <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
+                <strong class="font-medium">JWT_Token</strong>
 
-                            <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                                <strong class="font-medium">ToDo List</strong>
+                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
 
-                                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+                <p class="mt-0.5 opacity-50 sm:mt-0">Form / useFormik</p>
+              </div>
+            </a>
 
-                                <p class="mt-0.5 opacity-50 sm:mt-0">Vannila Javascript</p>
-                            </div>
-                        </a> */}
-                        {/* phrase=====2 */}
-                        <a href="https://mern-jwt-token.vercel.app/" target='blank' rel="noreferrer" class="block">
-                            <img
-                                alt="Signage"
-                                src={LogIn}
-                                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                            />
+            <a href="/" rel="noreferrer" class="block">
+              <img
+                alt="Signage"
+                src={eCommerce}
+                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
+              />
 
-                            <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                                <strong class="font-medium">JWT_Token</strong>
+              <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
+                <strong class="font-medium">E-Commerce</strong>
 
-                                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
 
-                                <p class="mt-0.5 opacity-50 sm:mt-0">Form / useFormik</p>
-                            </div>
-                        </a>
+                <p class="mt-0.5 opacity-50 sm:mt-0">Next.js / TypeScript</p>
+              </div>
+            </a>
 
-                        <a href="/" rel="noreferrer" class="block">
-                            <img
-                                alt="Signage"
-                                src={eCommerce}
-                                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                            />
+            <a
+              href="https://dr-kavis-womens-care-center-4yy4.vercel.app/"
+              target="blank"
+              rel="noreferrer"
+              class="block"
+            >
+              <img
+                alt="Signage"
+                src={Kavis}
+                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
+              />
 
-                            <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                                <strong class="font-medium">E-Commerce</strong>
+              <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
+                <strong class="font-medium">Dr.Kavi's Women Care center</strong>
 
-                                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
 
-                                <p class="mt-0.5 opacity-50 sm:mt-0">Next.js / TypeScript</p>
-                            </div>
-                        </a>
+                <p class="mt-0.5 opacity-50 sm:mt-0">RealTime / Nodemailer</p>
+              </div>
+            </a>
+            {/* phrase===3 */}
 
-                        <a href="https://dr-kavis-womens-care-center-4yy4.vercel.app/" target='blank' rel="noreferrer" class="block">
-                            <img
-                                alt="Signage"
-                                src={Kavis}
-                                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                            />
+            <a
+              href="https://play.google.com/store/apps?utm_source=apac_med&hl=en-IN&utm_medium=hasem&utm_content=Oct0121&utm_campaign=Evergreen&pcampaignid=MKT-EDR-apac-in-1003227-med-hasem-ap-Evergreen-Oct0121-Text_Search_BKWS-BKWS%7cONSEM_kwid_43700064490253544_creativeid_480915691381_device_c&gclid=Cj0KCQjw3JanBhCPARIsAJpXTx5WhhrIOQ3earlJfPYMSEpR3ywdSZum90xQ8e3MyEv8cu2AAHHwzUMaAtc5EALw_wcB&gclsrc=aw.ds"
+              target="blank"
+              rel="noreferrer"
+              class="block"
+            >
+              <img
+                alt="Signage"
+                src={Habits}
+                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
+              />
 
-                            <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                                <strong class="font-medium">Dr.Kavi's Women Care center</strong>
+              <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
+                <strong class="font-medium">Habits</strong>
 
-                                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
 
-                                <p class="mt-0.5 opacity-50 sm:mt-0">RealTime / Nodemailer</p>
-                            </div>
-                        </a>
-                        {/* phrase===3 */}
-                        {/* <a href="https://www.javatpoint.com/javascript-tutorial" target='blank' rel="noreferrer" class="block">
-                            <img
-                                alt="Signage"
-                                src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-                                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                            />
+                <p class="mt-0.5 opacity-50 sm:mt-0">
+                  Financial Mobile App/ ReactNaive
+                </p>
+              </div>
+            </a>
 
-                            <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                                <strong class="font-medium">Current Loacation</strong>
+            <a href="/" rel="noreferrer" class="block">
+              <img
+                alt="Signage"
+                src={Netflix}
+                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
+              />
 
-                                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
+              <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
+                <strong class="font-medium">Netflix</strong>
 
-                                <p class="mt-0.5 opacity-50 sm:mt-0">MERN / City_State API</p>
-                            </div>
-                        </a> */}
+                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
 
-                        <a href="https://play.google.com/store/apps?utm_source=apac_med&hl=en-IN&utm_medium=hasem&utm_content=Oct0121&utm_campaign=Evergreen&pcampaignid=MKT-EDR-apac-in-1003227-med-hasem-ap-Evergreen-Oct0121-Text_Search_BKWS-BKWS%7cONSEM_kwid_43700064490253544_creativeid_480915691381_device_c&gclid=Cj0KCQjw3JanBhCPARIsAJpXTx5WhhrIOQ3earlJfPYMSEpR3ywdSZum90xQ8e3MyEv8cu2AAHHwzUMaAtc5EALw_wcB&gclsrc=aw.ds" target='blank' rel="noreferrer" class="block">
-                            <img
-                                alt="Signage"
-                                src={Habits}
-                                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                            />
+                <p class="mt-0.5 opacity-50 sm:mt-0">
+                  {" "}
+                  ReduxToolkit / Social Media App
+                </p>
+              </div>
+            </a>
 
-                            <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                                <strong class="font-medium">Habits</strong>
-
-                                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
-                                <p class="mt-0.5 opacity-50 sm:mt-0">Financial Mobile App/ ReactNaive</p>
-                            </div>
-                        </a>
-
-                        <a href="/"  rel="noreferrer" class="block">
-                            <img
-                                alt="Signage"
-                                src={Netflix}
-                                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                            />
-
-                            <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                                <strong class="font-medium">Netflix</strong>
-
-                                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
-                                <p class="mt-0.5 opacity-50 sm:mt-0"> ReduxToolkit / Social Media App</p>
-                            </div>
-                        </a>
-
-                        {/* phrase===4 */}
-
-                        {/* <a href="https://www.javatpoint.com/javascript-tutorial" target='blank' rel="noreferrer" class="block">
-                            <img
-                                alt="Signage"
-                                src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-                                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                            />
-
-                            <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                                <strong class="font-medium">Chatting App</strong>
-
-                                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
-                                <p class="mt-0.5 opacity-50 sm:mt-0">Node.js / Socket.io</p>
-                            </div>
-                        </a>
-
-                        <a href="https://www.javatpoint.com/javascript-tutorial" target='blank' rel="noreferrer" class="block">
-                            <img
-                                alt="Signage"
-                                src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-                                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                            />
-
-                            <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                                <strong class="font-medium">Loacation</strong>
-
-                                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
-                                <p class="mt-0.5 opacity-50 sm:mt-0">MERN / City_State API</p>
-                            </div>
-                        </a>
-
-                        <a href="https://www.javatpoint.com/javascript-tutorial" target='blank' rel="noreferrer" class="block">
-                            <img
-                                alt="Signage"
-                                src="https://images.unsplash.com/photo-1588515724527-074a7a56616c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80"
-                                class="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-72"
-                            />
-
-                            <div class="mt-4 sm:flex sm:items-center sm:justify-center sm:gap-4">
-                                <strong class="font-medium">Payment Gateway</strong>
-
-                                <span class="hidden sm:block sm:h-px sm:w-8 sm:bg-yellow-500"></span>
-
-                                <p class="mt-0.5 opacity-50 sm:mt-0">Banking / Transactions</p>
-                            </div>
-                        </a> */}
-                    </div>
-                </div>
-            </section>
+            {/* phrase===4 */}
+          </div>
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
